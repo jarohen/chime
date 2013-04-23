74a3bd372daf085fadeb6427fc6455aed7380027
James Henderson <james@pcr.me.uk>
2013-04-23 21:08:25 +0100

    Adding clj-time dep

--- project.clj
+++ project.clj
@@ -3,4 +3,5 @@
   :url "https://github.com/james-henderson/chime.git"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.5.1"]])
+  :dependencies [[org.clojure/clojure "1.5.1"]
+                 [clj-time "0.5.0"]])
