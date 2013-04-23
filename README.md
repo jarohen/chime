f3280431ec11e60ffbfdd4ef8a04fea67fb9d836
James Henderson <james@pcr.me.uk>
2013-04-23 20:16:28 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,13 @@
+# Chime
+
+A really lightweight Clojure scheduler.
+
+## Usage
+
+FIXME
+
+## License
+
+Copyright © 2013 James Henderson
+
+Distributed under the Eclipse Public License, the same as Clojure.
---
+++ project.clj
@@ -0,0 +1,6 @@
+(defproject chime "0.1.0-SNAPSHOT"
+  :description "A really lightweight Clojure scheduler"
+  :url "https://github.com/james-henderson/chime.git"
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :dependencies [[org.clojure/clojure "1.5.1"]])
