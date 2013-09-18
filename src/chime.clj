4c2dee4457091ce5bbed1a6c91a4b5024256196a
James Henderson <james@pcr.me.uk>
2013-09-18 19:35:25 +0100

    Improving the error handling - can now pass an error-handling fn (defaulting to .printStackTrace)

--- src/chime.clj
+++ src/chime.clj
@@ -25,7 +25,7 @@
 (defn- split-at-now [times]
   (split-with #(t/before? % (*now*)) times))
 
-(defn- tick-handler [ag scheduler f]
+(defn- tick-handler [ag scheduler f error-handler]
   (fn handle-tick [times]
     (when (seq times)
 
@@ -33,7 +33,10 @@
 
         ;; run the fn for any times that have passed
         (doseq [time past-times]
-          (f time))
+          (try
+            (f time)
+            (catch Exception e
+              (error-handler e))))
 
         ;; reschedule this fn if we have more times
         (if (seq future-times)
@@ -43,11 +46,12 @@
         ;; and keep the future times for the next invocation
         future-times))))
 
-(defn chime-at [times f]
+(defn chime-at [times f & [{:keys [error-handler]
+                            :or {error-handler #(.printStackTrace %)}}]]
   (let [future-times (drop-while #(t/before? % (*now*)) times)
         ag (agent future-times)]
 
-    (send-off ag (tick-handler ag (new-scheduler) f))
+    (send-off ag (tick-handler ag (new-scheduler) f error-handler))
     
     (fn cancel-schedule []
       (send-off ag (constantly [])))))
@@ -59,3 +63,8 @@
              (-> 3 t/secs t/from-now)
              (-> 5 t/secs t/from-now)]
             #(println "Chiming!" %)))
+
+(comment
+  (chime-at [(-> 2 t/secs t/from-now)
+             (-> 3 t/secs t/from-now)]
+            #(throw (Exception. (format "Failing at time %s." %)))))
