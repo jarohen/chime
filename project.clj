90e5ec7fbc3d22bb3617659ca32a38c510a9aac9
James Henderson <james@pcr.me.uk>
2013-12-07 17:53:38 +0000

    Making chime use core.async channels - decimating LoC count!
    Fixes #2

--- project.clj
+++ project.clj
@@ -4,4 +4,5 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.5.1"]
-                 [clj-time "0.5.0"]])
+                 [clj-time "0.6.0"]
+                 [org.clojure/core.async "0.1.242.0-44b1e3-alpha"]])
