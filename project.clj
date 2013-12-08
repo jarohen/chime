f341666ca166e4a6edf6856bd3472c00969059ca
James Henderson <james@pcr.me.uk>
2013-12-08 20:21:03 +0000

    Bumping version to 0.1.3

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject jarohen/chime "0.1.2"
+(defproject jarohen/chime "0.1.3"
   :description "A really lightweight Clojure scheduler"
   :url "https://github.com/james-henderson/chime.git"
   :license {:name "Eclipse Public License"
