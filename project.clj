c54e470aea000eb4236dd0d76c471d59eaf59a45
James Henderson <james@jarohen.me.uk>
2017-06-03 15:37:37 +0100

    Release 0.2.2

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject jarohen/chime "0.2.1"
+(defproject jarohen/chime "0.2.2"
   :description "A really lightweight Clojure scheduler"
   :url "https://github.com/james-henderson/chime.git"
   :license {:name "Eclipse Public License"
