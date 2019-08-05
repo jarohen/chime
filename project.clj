a92fe07653b45086b4037ae5d69a31053eb53938
vemv <vemv@users.noreply.github.com>
2019-08-05 09:40:35 +0200

    Adapt comment-based tests into clojure.test ones

--- project.clj
+++ project.clj
@@ -5,4 +5,5 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.7.0"]
                  [clj-time/clj-time "0.11.0"]
-                 [org.clojure/core.async "0.3.442"]])
+                 [org.clojure/core.async "0.3.442"]]
+  :profiles {:dev {:source-paths ["test"]}})
