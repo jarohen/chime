76544ef510000b3cc1bebeea2dbaa15dcaf3ecfb
James Henderson <james@jarohen.me.uk>
2020-01-22 13:02:39 +0000

    bump versions

--- project.clj
+++ project.clj
@@ -3,7 +3,7 @@
   :url "https://github.com/james-henderson/chime.git"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.7.0"]
-                 [clj-time/clj-time "0.11.0"]
-                 [org.clojure/core.async "0.3.442"]]
+  :dependencies [[org.clojure/clojure "1.10.1"]
+                 [clj-time/clj-time "0.15.2"]
+                 [org.clojure/core.async "0.7.559"]]
   :profiles {:dev {:source-paths ["test"]}})
