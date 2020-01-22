8d629a354cec199026e291c27b1ebd8f3ff4b7e8
James Henderson <james@jarohen.me.uk>
2020-01-22 13:20:23 +0000

    adding to Changelog, bump to SNAPSHOT

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject jarohen/chime "0.2.2"
+(defproject jarohen/chime "0.3.0-SNAPSHOT"
   :description "A really lightweight Clojure scheduler"
 
   :url "https://github.com/james-henderson/chime.git"
