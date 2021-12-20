fa0b6e8c0f68e6d6134aee6ba9eb2ce032ba65b0
James Henderson <james@jarohen.dev>
2021-12-20 10:26:06 +0000

    bump deps, #52

--- project.clj
+++ project.clj
@@ -6,7 +6,7 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
 
-  :dependencies [[org.clojure/clojure "1.10.1"]
-                 [org.clojure/tools.logging "1.0.0"]
+  :dependencies [[org.clojure/clojure "1.10.3"]
+                 [org.clojure/tools.logging "1.2.3"]
                  [clj-time/clj-time "0.15.2" :scope "provided"]
-                 [org.clojure/core.async "1.1.587" :scope "provided"]])
+                 [org.clojure/core.async "1.5.648" :scope "provided"]])
